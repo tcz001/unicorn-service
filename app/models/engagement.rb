975d7f0dd4ce9ac0560eb41858743185fd369569
Fan Jiang <fanjiang@thoughtworks.com>
2014-07-22 13:26:18 +0800

    modify time limits

--- app/models/engagement.rb
+++ app/models/engagement.rb
@@ -1,5 +1,5 @@
 class Engagement < ActiveRecord::Base
   belongs_to :beacon
   belongs_to :unicorn
-  default_scope {where ("engagements.created_at >= '#{(Time.zone.now - 10.minute).to_s(:db)}'")}
+  default_scope {where ("engagements.created_at >= '#{(Time.zone.now - 60.seconds).to_s(:db)}'")}
 end
