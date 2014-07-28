95a2335b32e771018be5ccbeefe7bb8c73823240
Fan Jiang <fanjiang@thoughtworks.com>
2014-07-28 21:48:21 +0800

    change log config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.time_zone = 'Beijing'
+  config.logger = Logger.new(STDOUT)
 end
